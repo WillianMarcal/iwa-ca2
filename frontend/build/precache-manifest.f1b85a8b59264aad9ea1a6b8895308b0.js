97623efc1e6e8af7049964d99e9e08c95ef49018
Willian Marcal <willianpetermarcal@gmail.com>
2022-05-13 15:07:24 +0100

    version2_build_data_package.jason_package-lockjason

---
+++ frontend/build/precache-manifest.f1b85a8b59264aad9ea1a6b8895308b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d34fcf785ae3a89fa18f87cdf87a7f63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "205955f0038f5b723580",
+    "url": "/static/css/main.458d3275.chunk.css"
+  },
+  {
+    "revision": "411749d26dfce51f7355",
+    "url": "/static/js/2.3343c7bc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3343c7bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "205955f0038f5b723580",
+    "url": "/static/js/main.556e8d30.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
